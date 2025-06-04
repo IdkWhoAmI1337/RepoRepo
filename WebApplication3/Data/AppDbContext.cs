b0d7c7d16e47224f0afce36855d247e582568e2a
Bartosz <s31218@pjwstk.edu.pl>
2025-06-04 18:53:41 +0200

    fsafasfs

--- WebApplication3/Data/AppDbContext.cs
+++ WebApplication3/Data/AppDbContext.cs
@@ -9,4 +9,44 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
     public DbSet<Concert> Concerts { get; set; }
     public DbSet<Ticket> Tickets { get; set; }
     public DbSet<PurchasedTicket> PurchasedTickets { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Concert>().HasData(
+            new Concert
+            {
+                ConcertId = 1,
+                Name = "Summer Rock Festival",
+                Date = new DateTime(2025, 7, 15),
+                AvailableTickets = 150
+            }
+        );
+        modelBuilder.Entity<Customer>().HasData(
+            new Customer
+            {
+                CustomerId = 1,
+                FirstName = "Jan",
+                LastName = "Kowalski",
+                PhoneNumber = "555-123-456"
+            }
+        );
+        modelBuilder.Entity<Ticket>().HasData(
+            new Ticket
+            {
+                TicketId = 1,
+                SerialNumber = "SN123456",
+                SeatNumber = 12,
+            }
+        );
+        modelBuilder.Entity<TicketConcert>().HasData(
+            new TicketConcert
+            {
+                TicketConcertId = 1,
+                TicketId = 1,
+                ConcertId = 1,
+                Price = 99.99
+            }
+        );
+    }
+    
 }
