5597f611902b86314e7844015cdf7776802ddd4e
Bartosz <s31218@pjwstk.edu.pl>
2025-06-11 18:18:04 +0200

    get

--- WebApplication3/Services/CharacterService.cs
+++ WebApplication3/Services/CharacterService.cs
@@ -1,5 +1,7 @@
-﻿using WebApplication3.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication3.Data;
 using WebApplication3.DTOs;
+using WebApplication3.Models;
 
 namespace WebApplication3.Services;
 
@@ -13,7 +15,39 @@ public class CharacterService(AppDbContext context) : ICharacterService
 {
     public async Task<CharacterGetDto> GetCharacterById(int characterId)
     {
-        return new CharacterGetDto{};
+        var character = await context.Characters
+            .Include(c => c.Backpacks)
+            .ThenInclude(b => b.Item)
+            .Include(c => c.CharacterTitles)
+            .ThenInclude(t => t.Title)
+            .FirstOrDefaultAsync(c => c.CharacterId == characterId);
+
+        if (character == null)
+        {
+            throw new ArgumentException("Character not found");
+        }
+        
+        var backpackItems = character.Backpacks.Select(backpack => new CharacterGetDto.BackpackItem
+        {
+            ItemName = backpack.Item.Name,
+            ItemWeight = backpack.Item.Weight,
+            Amount = backpack.Amount
+        }).ToList();
+        var titles = character.CharacterTitles.Select(t => new CharacterGetDto.Title
+        {
+            Name = t.Title.Name,
+            AquiredAt = t.AcquiredAt
+        }).ToList();
+
+        return new CharacterGetDto
+        {
+            FirstName = character.FirstName,
+            LastName = character.LastName,
+            CurrentWeight = character.CurrentWeight,
+            MaxWeight = character.MaxWeight,
+            BackpackItems = backpackItems,
+            Titles = titles
+        };
     }
 
     public async Task<int> AddItemsToBackpack(int characterId, CharacterPostDto request)
